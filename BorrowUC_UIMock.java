2fbac104edbfd7155eee60240a57895962b44da0
Chamara <chamarabandara843@gmail.com>
2016-10-06 00:19:10 +1100

    BorrowUC_UI

---
+++ BorrowUC_UIMock.java
@@ -0,0 +1,16 @@
+package src.library;
+
+import javax.annotation.Generated;
+
+/** Mock for { @link BorrowUC_UI } */
+@Generated(value = "org.junit-tools-1.0.3")
+public class BorrowUC_UIMock extends MockUp<BorrowUC_UI> {
+
+	public static BorrowUC_UIMock create() {
+		return new BorrowUC_UIMock();
+	}
+
+	public void resetAllMocks() {
+
+	}
+}
