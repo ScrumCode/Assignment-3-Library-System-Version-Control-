6cf57f2670a01cb60c743c4a60bd0d1b9e99da2e
Pravin <pravindilshan@gmail.com>
2016-10-06 00:17:37 +1100

    BorrowUC_CTL
    Original and MockClass

---
+++ BorrowUC_CTLMock.java
@@ -0,0 +1,16 @@
+package src.library;
+
+import javax.annotation.Generated;
+
+/** Mock for { @link BorrowUC_CTL } */
+@Generated(value = "org.junit-tools-1.0.3")
+public class BorrowUC_CTLMock extends MockUp<BorrowUC_CTL> {
+
+	public static BorrowUC_CTLMock create() {
+		return new BorrowUC_CTLMock();
+	}
+
+	public void resetAllMocks() {
+
+	}
+}
