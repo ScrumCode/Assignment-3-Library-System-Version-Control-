bec16d8e8d3c101c9732874247d1e8814508c411
Chamara <chamarabandara843@gmail.com>
2016-10-05 23:42:24 +1100

    EbookState and ELoanState
    EbookState and ELoanState classes with their mock files

---
+++ EBookStateMock.java
@@ -0,0 +1,16 @@
+package src.library.interfaces.entities;
+
+import javax.annotation.Generated;
+
+/** Mock for { @link EBookState } */
+@Generated(value = "org.junit-tools-1.0.3")
+public class EBookStateMock extends MockUp<EBookState> {
+
+	public static EBookStateMock create() {
+		return new EBookStateMock();
+	}
+
+	public void resetAllMocks() {
+
+	}
+}
---
+++ ELoanStateMock.java
@@ -0,0 +1,16 @@
+package src.library.interfaces.entities;
+
+import javax.annotation.Generated;
+
+/** Mock for { @link ELoanState } */
+@Generated(value = "org.junit-tools-1.0.3")
+public class ELoanStateMock extends MockUp<ELoanState> {
+
+	public static ELoanStateMock create() {
+		return new ELoanStateMock();
+	}
+
+	public void resetAllMocks() {
+
+	}
+}
