13a2931fe154163201dfdad7420c96a19f70a391
Sankalpa <sankalpa1994@gmail.com>
2016-10-06 00:19:56 +1100

    Main

---
+++ MainMock.java
@@ -0,0 +1,16 @@
+package src.library;
+
+import javax.annotation.Generated;
+
+/** Mock for { @link Main } */
+@Generated(value = "org.junit-tools-1.0.3")
+public class MainMock extends MockUp<Main> {
+
+	public static MainMock create() {
+		return new MainMock();
+	}
+
+	public void resetAllMocks() {
+
+	}
+}
